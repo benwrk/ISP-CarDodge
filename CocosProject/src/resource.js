88a2d85be8450af284cbee126cacc5de49050488
Benjapol Worakan <benwrk@live.com>
2015-05-08 02:49:13 +0700

    Cars added.

--- CocosProject/src/resource.js
+++ CocosProject/src/resource.js
@@ -1,5 +1,10 @@
 var res = {
     //HelloWorld_png : "res/HelloWorld.png",
+	Car_Blue_png : "res/Car-Blue.png",
+	Car_Green_png : "res/Car-Green.png",
+	Car_Red_png : "res/Car-Red.png",
+	Car_White_png : "res/Car-White.png",
+	Car_Yellow_png : "res/Car-Yellow.png"
 };
 
 var g_resources = [];
